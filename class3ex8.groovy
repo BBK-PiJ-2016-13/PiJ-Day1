51d635c3bacd06c9224924cae761086376811fb7
Eileen Parle <eileenlny@gmail.com>
2016-10-30 20:57:07 +0000

    did the second part of exercise 8 from day 3

--- class3ex8.groovy
+++ class3ex8.groovy
@@ -1,7 +1,15 @@
 println "Input some characters please "
 String str1 = System.console().readLine()
+str1 = str1 + " "
 int count = 0
+String strword = ""
 while (count < str1.length()) {
-	println str1[count]
-	count ++
+	if (str1[count] != " ") {
+		strword = strword + str1[count];
+		count ++;
+	} else {
+		println strword;
+		strword = "";
+		count ++;
+	}
 }
